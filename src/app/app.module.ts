e1482b925e137881c68d3e852b553fbab7b64e7d
suchat-kl <suchat.kl@kkumail.com>
2021-05-25 16:10:51 +0700

    complete lab4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Lab4Component } from './lab4/lab4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lab4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lab4/lab4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab4',
+  templateUrl: './lab4.component.html',
+  styleUrls: ['./lab4.component.css']
+})
+export class Lab4Component implements OnInit {
+items =[1,2,3,4,5,6,7,8,9,10];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
